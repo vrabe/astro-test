75193f44c7573db1ed62abbe8b51c7e32cbd61c7
vrabe <git@vrabe.tw>
2025-08-04 22:11:36 +0800

    test3

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://vrabe.github.io',
+	base: 'astro-test',
 	integrations: [mdx(), sitemap()],
 });
